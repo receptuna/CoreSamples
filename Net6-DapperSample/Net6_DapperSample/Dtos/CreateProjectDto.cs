ba39205bd79de76da1a91bd51f35022ceefb3d1c
receptuna <receptuna@live.com>
2024-03-06 01:51:11 +0300

    dapper project sample

---
+++ Net6-DapperSample/Net6_DapperSample/Dtos/CreateProjectDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Net6_DapperSample.Dtos
+{
+    public class CreateProjectDto
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int ProjectCategory { get; set; }
+        public int CompleteDay { get; set; }
+        public decimal Price { get; set; }
+    }
+}
